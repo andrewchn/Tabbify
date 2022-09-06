7b07d6a3cc7ef32704153ffacc4a6898a4848a5b
Claire Liu <claire.l.liu1@gmail.com>
2022-09-05 22:57:25 -0400

    disabled linters

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
 
+// eslint-disable-next-line no-undef
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
